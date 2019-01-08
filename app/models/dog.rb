c83d08fc9fff23266338c1be760cc1238ca54af5
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-01-07 21:28:56 -0500

    Interim checkin

---
+++ app/models/dog.rb
@@ -0,0 +1,11 @@
+class Dog < Sinatra::Base
+attr_accessor :name, :age
+attr_reader :breed
+@@all = []
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
+  end
+end
